00316c39a7a5e836411e52ba2412507ba333133d
scoutmark10-ui <scoutmark10@gmail.com>
2026-06-27 10:24:47 +0200

    first commit

---
+++ public/js/index.js
@@ -0,0 +1,191 @@
+function sucesso(msg) {
+    Toastify({
+        text: msg,
+        duration: 3000,
+        gravity: "top",
+        position: "right",
+        style: {
+            background: "linear-gradient(to right, #00b09b, #96c93d)"
+        }
+    }).showToast();
+}
+
+function erro(msg) {
+    Toastify({
+        text: msg,
+        duration: 3000,
+        gravity: "top",
+        position: "right",
+        style: {
+            background: "linear-gradient(to right, #ff416c, #ff4b2b)"
+        }
+    }).showToast();
+}
+
+function aviso(msg) {
+    Toastify({
+        text: msg,
+        duration: 3000,
+        gravity: "top",
+        position: "right",
+        style: {
+            background: "linear-gradient(to right, #f7971e, #ffd200)"
+        }
+    }).showToast();
+}
+
+function warning(msg) {
+    return Swal.fire({
+        icon: "warning",
+        title: "Atenção",
+        text: msg,
+        showCancelButton: true,
+        confirmButtonText: "Sim",
+        cancelButtonText: "Cancelar",
+        confirmButtonColor: "#e74c3c"
+    });
+}
+
+// ================================
+// renderizar
+// ================================
+function renderizarAnimais(lista) {
+    const container = document.getElementById("lista-animais");
+    container.innerHTML = "";
+
+    lista.forEach(animal => {
+        const li = document.createElement("li");
+
+        li.innerHTML = `
+      <strong>${animal.nome}</strong><br>
+      Idade: ${animal.idade}<br>
+      Sexo: ${animal.sexo}<br>
+      Cor: ${animal.cor}<br><br>
+
+      <button onclick="eliminar('${animal.id}')">
+        Eliminar
+      </button>
+    `;
+
+        container.appendChild(li);
+    });
+}
+
+// ================================
+// carregar animais
+// ================================
+async function carregarAnimais() {
+    const container = document.getElementById("lista-animais");
+    const response = await axios.get("http://localhost:8000/animais");
+
+    todosAnimais = response.data;
+    // guarda original
+
+    container.innerHTML = "";
+
+if (todosAnimais.length === 0) {
+  container.innerHTML = "<p>Nenhum animal cadastrado</p>";
+  return;
+}
+
+    renderizarAnimais(todosAnimais);
+}
+
+// ================================
+// cadastrar animais
+// ================================
+function guardarAnimal() {
+    const form_animal = document.getElementById("form_animal");
+    const input_nome = document.getElementById("nome");
+
+    form_animal.onsubmit = async e => {
+        e.preventDefault();
+        const nome = document.getElementById("nome").value;
+        const idade = document.getElementById("idade").value;
+        const sexo = document.getElementById("sexo").value;
+        const cor = document.getElementById("cor").value;
+
+        if (idade <= 0) {
+            erro("Idade inválida");
+        } else {
+            await axios.post("http://localhost:8000/animais", {
+                nome: nome,
+                idade: idade,
+                sexo: sexo,
+                cor: cor
+            });
+
+            sucesso("Animal cadastrado com sucesso");
+        }
+
+        carregarAnimais();
+    };
+}
+
+// ================================
+// buscar animal
+// ================================
+function buscarAnimal() {
+    const nome = document.getElementById("buscar").value.trim().toLowerCase();
+
+    if (nome.trim().length === 0) {
+        erro(`Digite o nome do animal`);
+        renderizarAnimais(todosAnimais);
+        return;
+    }
+
+    const filtrados = todosAnimais.filter(animal =>
+        animal.nome.toLowerCase().includes(nome)
+    );
+
+    if (filtrados.length === 0) {
+        erro(`O animal ${nome} não foi encontrado`);
+        renderizarAnimais(todosAnimais);
+        return;
+    }
+
+    renderizarAnimais(filtrados);
+}
+
+// ================================
+// resetar busca feita
+// ================================
+function resetLista() {
+    document.getElementById("buscar").value = "";
+    renderizarAnimais(todosAnimais);
+    sucesso("Lista de busca resetada com sucesso");
+}
+
+// ================================
+// eliminar animais
+// ================================
+async function eliminar(id) {
+    const result = await warning("Tens a certeza que queres eliminar este animal?");
+
+    if (!result.isConfirmed) {
+        return;
+    }
+
+    try {
+        await axios.delete(`http://localhost:8000/animais/${id}`);
+        sucesso("Animal eliminado com sucesso");
+
+        carregarAnimais();
+    } catch (error) {
+        console.error(error);
+        erro("Ocorreu um erro ao eliminar");
+    }
+}
+
+// ================================
+// aplicativo
+// ================================
+function app() {
+    console.log("App iniciada");
+    carregarAnimais();
+    guardarAnimal();
+    buscarAnimal();
+    eliminar(id);
+}
+
+app();
---
+++ src/server.py
@@ -0,0 +1,91 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from typing import List, Optional
+from pydantic import BaseModel
+from uuid import uuid4
+
+app = FastAPI(
+ title = "App Animal API",
+ description = "API criada por Zynko para estudos de FastAPI.",
+  version = "1.0.0"
+)
+
+origins = ['http://localhost:8000']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# estrutura de cada animal
+class Animal(BaseModel):
+  # id opcional igualamos a none
+  id: Optional[uuid4] = None
+  nome: str
+  idade: int
+  sexo: str
+  cor: str
+
+# banco de dados dos animais
+banco: List[Animal] = []
+
+# ver todos animais
+
+@app.get('/animais')
+def lista_animais():
+  return banco
+
+# obter animal
+
+@app.get('/animais/name')
+def encontrar_animal(nome: str):
+    encontrados = []
+
+    for animal in banco:
+        if animal.nome.lower() == nome.lower():
+            encontrados.append(animal)
+
+    if encontrados:
+        return {
+            "mensagem": "Animal encontrado com sucesso",
+            "encontrados": encontrados
+        }
+
+    return {
+        "mensagem": "Animal não encontrado",
+        "encontrados": []
+    }
+
+# criar animal
+
+@app.post('/animais')
+def criar_animal(animal: Animal):
+  # obtemos animais com ids aleatorios
+  animal.id = str(uuid4())
+  # tipo um push pra mandar o animal para o banco de dados
+  banco.append(animal)
+  return {
+    "mensagem": "Animal criado com sucesso",
+    "animal": animal
+}
+
+# deletar animal
+
+@app.delete('/animais/{id}')
+def remover_animal(id: str):
+  posicao = -1
+  
+  # buscando a posicao do animal
+  for index, animal in enumerate(banco):
+    if animal.id == id:
+      posicao = index
+      break
+
+  if posicao != -1:
+    banco.pop(posicao)
+    return {"mensagem": "Animal removido com sucesso"}
+  else:
+    return {"erro": "Animal não encontrado"}
